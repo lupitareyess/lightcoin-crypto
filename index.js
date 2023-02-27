dc64b6bac59ed97f633abb08dfd11b9aeddc006e
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-26 21:12:13 -0500

    Object oriented programming example problem

--- index.js
+++ index.js
@@ -1,29 +1,103 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
+// allow withdrawals from account
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+// allow deposits into accounts
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
+
+
+
+
+
+// Allow multiple accounts to be created - DONE
+// Each account can have many transactions - DONE
+// Allow withdrawals and deposits into accounts - DONE
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits) - DONE
+// Allow us to retrieve the current balance of the account at any time - DONE
+// Don't allow withdrawals that exceed the remaining balance of the account - DONE
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+//Create a new account for every user of the app
+const myAccount = new Account("Lupita");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Starting Account Balance: ', myAccount.balance);
 
-console.log('Balance:', balance);
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
+
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
+
